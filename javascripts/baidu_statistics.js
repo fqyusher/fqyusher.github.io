1c76404029da6224e7bf11c91834ce30b3f90dbd
yusher <fqyusher@gmail.com>
2018-11-30 17:00:14 +0800

    Feat: add google analytics

--- javascripts/baidu_statistics.js
+++
@@ -1,9 +0,0 @@
-<script>
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?2e71a7aee8f0df332dd16e1e5506da06";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
-</script>
